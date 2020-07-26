4f022f541a09ae80227e864d6a297941eff0b464
Adam Drozdz <adam.drozdz@iterative.pl>
2020-07-27 01:28:24 +0200

    fix user agent for tmobile bank services

--- src/ios/CDVWKInAppBrowser.m
+++ src/ios/CDVWKInAppBrowser.m
@@ -771,7 +771,7 @@ BOOL isExiting = FALSE;
     // if ([self settingForKey:@"OverrideUserAgent"] != nil) {
     //     self.webView.customUserAgent = [self settingForKey:@"OverrideUserAgent"];
     // }
-    self.webView.customUserAgent = @"Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148";
+    self.webView.customUserAgent = @"Mozilla/5.0 (iPad; CPU OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1";
     
     self.webView.clearsContextBeforeDrawing = YES;
     self.webView.clipsToBounds = YES;
